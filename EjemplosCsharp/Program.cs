ce8bd9a64b392430d7e2a4bf3bb1e1bdd0b7a129
rusokverse <rusokverse@MACBOOKPRO>
2023-01-30 22:02:43 -0400

    Agregar archivos de proyecto.

---
+++ EjemplosCsharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cosa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola mundo!");
+
+            Console.Read();
+        }
+    }
+}
+
+namespace otroNombre
+{
+    namespace unoDentro
+    {
+        
+    }
+}
